9f3963014270913d320199a4a2bb971417808c4e
hansraptor <hannes.swanepoel@disraptor.co.za>
2024-05-28 11:26:46 +0200

    remove comments

--- infrastructure/monolith/main.tf
+++ infrastructure/monolith/main.tf
@@ -53,11 +53,6 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = local.lambda_name
   principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  # source_arn = "${aws_api_gateway_rest_api.monolith_api.arn}/*/${aws_api_gateway_method.root_post_method.http_method}/"
-  
-  # source_arn = "arn:aws:execute-api:af-south-1:767398121047:${aws_api_gateway_rest_api.monolith_api.id}/*/${aws_api_gateway_method.root_post_method.http_method}${aws_api_gateway_resource.resource.path}"
   source_arn = "${aws_api_gateway_rest_api.monolith_api.execution_arn}/*/${aws_api_gateway_method.root_post_method.http_method}/"
 
   depends_on = [

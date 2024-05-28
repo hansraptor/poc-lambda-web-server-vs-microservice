74c3b0d0c048cf8adb3c4f752c97e2a71ef719c3
hansraptor <hannes.swanepoel@disraptor.co.za>
2024-05-28 13:45:18 +0200

    impmlement lambda web server - install express & serverless - implement node18 function architecture - invoke serverless as function handler - exclude package lock from function archive - update node runtime in lambda - add a description for the api - remove api root level resources - add proxy method and update references accordingly

---
+++ code/monolith/index.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const serverless = require("serverless-http");
+const webserver = express();
+const port = 3000;
+
+webserver.get("/test", (request, response) => {
+  console.log(`${request.method} ${request.url}`);
+
+  response.status(200).json({
+    message: `Hello from Terraformed Lambda!`,
+  })
+});
+
+webserver.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports.handler = serverless(webserver);
--- infrastructure/monolith/main.tf
+++ infrastructure/monolith/main.tf
@@ -22,7 +22,8 @@ data "archive_file" "function_zip" {
   excludes = [
     ".gitignore",
     "monolith.zip",
-    "package.json"
+    "package.json",
+    "package-lock.json"
   ]
   output_file_mode = "0666"
   output_path      = "${local.function_source_location}/monolith.zip"
@@ -31,7 +32,7 @@ data "archive_file" "function_zip" {
 resource "aws_lambda_function" "webserver_lambda" {
   function_name    = local.lambda_name
   role             = local.allow_write_log_group_stream_event_role_arn
-  runtime          = "nodejs20.x"
+  runtime          = "nodejs18.x"
   handler          = "index.handler"
   filename         = data.archive_file.function_zip.output_path
   source_code_hash = data.archive_file.function_zip.output_base64sha256
@@ -43,44 +44,68 @@ resource "aws_lambda_function" "webserver_lambda" {
 
 resource "aws_api_gateway_rest_api" "monolith_api" {
   name = local.api_name
+  description = "API wrapping a single endpoint integrating with a lambda-based web server to route and handle requests"
 }
 
-resource "aws_api_gateway_method" "root_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.monolith_api.id
-  resource_id   = aws_api_gateway_rest_api.monolith_api.root_resource_id
-  authorization = "NONE"
-  http_method   = "POST"
+resource "aws_api_gateway_resource" "greedy_proxy_resource" {
+  rest_api_id = aws_api_gateway_rest_api.monolith_api.id
+  parent_id = aws_api_gateway_rest_api.monolith_api.root_resource_id
+  path_part = "{proxy+}"
 
   depends_on = [
     aws_api_gateway_rest_api.monolith_api
   ]
 }
 
-resource "aws_api_gateway_integration" "root_post_lambda_integration" {
+# resource "aws_api_gateway_method" "root_post_method" {
+#   rest_api_id   = aws_api_gateway_rest_api.monolith_api.id
+#   resource_id   = aws_api_gateway_rest_api.monolith_api.root_resource_id
+#   authorization = "NONE"
+#   http_method   = "POST"
+
+#   depends_on = [
+#     aws_api_gateway_rest_api.monolith_api
+#   ]
+# }
+
+resource "aws_api_gateway_method" "proxy_any_method" {
+  rest_api_id = aws_api_gateway_rest_api.monolith_api.id
+  resource_id = aws_api_gateway_resource.greedy_proxy_resource.id
+  authorization = "NONE"
+  http_method = "ANY"
+
+  depends_on = [
+    aws_api_gateway_rest_api.monolith_api,
+    aws_api_gateway_resource.greedy_proxy_resource
+  ]
+}
+
+resource "aws_api_gateway_integration" "proxy_any_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.monolith_api.id
-  resource_id             = aws_api_gateway_rest_api.monolith_api.root_resource_id
-  http_method             = aws_api_gateway_method.root_post_method.http_method
+  resource_id             = aws_api_gateway_resource.greedy_proxy_resource.id
+  http_method             = aws_api_gateway_method.proxy_any_method.http_method
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri                     = aws_lambda_function.webserver_lambda.invoke_arn
 
   depends_on = [
     aws_api_gateway_rest_api.monolith_api,
-    aws_api_gateway_method.root_post_method,
+    aws_api_gateway_resource.greedy_proxy_resource,
+    aws_api_gateway_method.proxy_any_method,
     aws_lambda_function.webserver_lambda
   ]
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+  statement_id  = "AllowAPIGatewayLambdaProxyIntegration"
   action        = "lambda:InvokeFunction"
   function_name = local.lambda_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.monolith_api.execution_arn}/*/${aws_api_gateway_method.root_post_method.http_method}/"
+  source_arn    = "${aws_api_gateway_rest_api.monolith_api.execution_arn}/*/*/*"
 
   depends_on = [
     aws_api_gateway_rest_api.monolith_api,
-    aws_api_gateway_method.root_post_method
+    aws_api_gateway_method.proxy_any_method
   ]
 }
 
@@ -92,8 +117,8 @@ resource "aws_api_gateway_deployment" "default_deployment" {
   depends_on = [
     aws_lambda_function.webserver_lambda,
     aws_api_gateway_rest_api.monolith_api,
-    aws_api_gateway_method.root_post_method,
-    aws_api_gateway_integration.root_post_lambda_integration,
+    aws_api_gateway_method.proxy_any_method,
+    aws_api_gateway_integration.proxy_any_lambda_integration,
     aws_lambda_permission.apigw_lambda
   ]
 }
